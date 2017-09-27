330069506a7ee9d1c41809c536735b761ee50b32
zjt <jintao7080@gmail.com>
2017-09-27 10:27:53 +0800

    keepAlive

--- lib/gnet/net.go
+++ lib/gnet/net.go
@@ -1,10 +1,26 @@
 package gnet
 
 import (
+	"errors"
 	"net"
+	"time"
 
 	"github.com/u35s/gmod/lib/utils"
 )
+
+func ConnectTo(addr string) (*net.TCPConn, error) {
+	c, err := net.DialTimeout("tcp", addr, 2*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	conn, ok := c.(*net.TCPConn)
+	if !ok {
+		return nil, errors.New("conn to tcpconn err")
+	}
+	conn.SetKeepAlivePeriod(time.Second)
+	conn.SetKeepAlive(true)
+	return conn, nil
+}
 
 func Listen(addr string) (*net.TCPListener, error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", addr)
@@ -23,11 +39,13 @@ func Listen(addr string) (*net.TCPListener, error) {
 
 func Accept(listener *net.TCPListener, f func(conn net.Conn)) {
 	for {
-		conn, err := listener.Accept()
+		conn, err := listener.AcceptTCP()
 		if err != nil {
 			utils.Err("accept listener %v err,%v", listener.Addr(), err)
 			break
 		}
+		conn.SetKeepAlivePeriod(time.Second)
+		conn.SetKeepAlive(true)
 		go f(conn)
 	}
 }
